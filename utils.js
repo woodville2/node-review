8cfb0ca2727187dda90216f64cb546e9f6153439
lori <woodies32005@gmail.com>
2025-04-18 21:04:03 -0500

    working on assignment

---
+++ utils.js
@@ -0,0 +1,11 @@
+"use strict";
+
+export function greet(name){
+   return("howdy," + name)
+}
+console.log(greet("elle"));
+
+export function favoriteCar(brand){
+   return("favorite car," + brand)
+}
+console.log(favoriteCar("Toyota"));
